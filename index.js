4c4472da4cd810e9fcfb1f55cd7252c82d82c43f
Mark Harmsen <mark.harmsen@woperty.com>
2020-06-23 08:54:13 -0400

    Enhance scraper speed. Part 2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json())                         // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 const PUPPETEER_OPTIONS = {
-  headless: false,
+  headless: true,
   timeout: 30000,
   args: [
     '--disable-gpu',
@@ -38,28 +38,24 @@ const closeConnection = async (page, browser) => {
   page && (await page.close());
   browser && (await browser.close());
 };
-/**
- * Request to servipag
- */
 
 app.post('/servipag', async (req, res) => {
-  const url = 'https://ww3.servipag.com/pagoenlinea/portal/desktop/public/generico'
 
-  if(req.body.type && req.body.company) {
-
+  if(req.body.client && req.body.company) {
+    const url = 'https://ww3.servipag.com/pagoenlinea/portal/desktop/public/generico'
     let { browser, page } = await openConnection();
     try {
       await page.goto(url, { waitUntil: 'load' });
-
+  
       await page.waitForSelector('#buscaempresa')
       await page.click('#buscaempresa')
       await page.keyboard.type(req.body.company)
-
+  
       await page.waitForSelector('#scrollMod li:nth-child(2)')
       await page.click('#scrollMod li:nth-child(2)')
-
+  
       await page.waitForSelector('.middle-service')
-
+  
       const companyId = await page.evaluate(params => {
         const options = document.querySelectorAll('.middle-service select>option')
         for (var i = 0; i < options.length; i++) {
@@ -68,13 +64,13 @@ app.post('/servipag', async (req, res) => {
           }
         }
       }, {companyName: req.body.company})
-
+  
       await page.waitForSelector(`input#input_${companyId}`)
       await page.click(`input#input_${companyId}`)
       await page.keyboard.type(req.body.client)
       await page.click('.next-cart-step')
       // await page.waitFor(30000)
-
+  
       try {
         await page.waitForSelector('.cost-detail')
         const amount = await page.$eval('.cost-detail', el => el.innerText)
@@ -90,7 +86,7 @@ app.post('/servipag', async (req, res) => {
         })
       } catch (error) {
         await page.waitForSelector('#help-modal')
-
+  
         const amount = await page.$eval('#help-modal p', el => el.innerText)
         console.log({
           amount: amount
@@ -100,11 +96,11 @@ app.post('/servipag', async (req, res) => {
           amount: amount
         })
       }
-
+  
     } catch (error) {
       console.log(error)
       return res.json({
-          error
+        error
       })
     } finally {
       await closeConnection(page, browser);
@@ -117,86 +113,86 @@ app.post('/servipag', async (req, res) => {
 
 })
 
-app.post('/servipag-old', async (req, res) => {
-  const url = 'https://ww3.servipag.com/pagoenlinea/portal/desktop/public/todas'
+// app.post('/servipag-old', async (req, res) => {
+//   const url = 'https://ww3.servipag.com/pagoenlinea/portal/desktop/public/todas'
 
-  if(req.body.client && req.body.type && req.body.company) {
+//   if(req.body.client && req.body.type && req.body.company) {
 
-    let { browser, page } = await openConnection();
-    try {
-      await page.goto(url, { waitUntil: 'load' });
+//     let { browser, page } = await openConnection();
+//     try {
+//       await page.goto(url, { waitUntil: 'load' });
 
-      await page.waitForSelector('#nom_serv0')
+//       await page.waitForSelector('#nom_serv0')
 
-      const serviceId = await page.evaluate(type => {
-        const hFour = document.querySelectorAll("h4")
-        for (var i = 0; i < hFour.length; i++) {
-          if (hFour[i].textContent == type) {
-            return hFour[i].id.replace('nom_serv', '')
-          }
-        }
-      }, req.body.type)
+//       const serviceId = await page.evaluate(type => {
+//         const hFour = document.querySelectorAll("h4")
+//         for (var i = 0; i < hFour.length; i++) {
+//           if (hFour[i].textContent == type) {
+//             return hFour[i].id.replace('nom_serv', '')
+//           }
+//         }
+//       }, req.body.type)
 
-      await page.click(`h4#nom_serv${serviceId}`)
+//       await page.click(`h4#nom_serv${serviceId}`)
 
-      const selectId = `biller_${serviceId}`
-      const companyId = await page.evaluate(params => {
-        const options = document.querySelectorAll(`#${params.id}>option`)
-        for (var i = 0; i < options.length; i++) {
-          if (options[i].textContent == params.companyName) {
-            return options[i].getAttribute('value')
-          }
-        }
-      }, {id: selectId, companyName: req.body.company})
-      await page.select(`select#${selectId}`, companyId),
+//       const selectId = `biller_${serviceId}`
+//       const companyId = await page.evaluate(params => {
+//         const options = document.querySelectorAll(`#${params.id}>option`)
+//         for (var i = 0; i < options.length; i++) {
+//           if (options[i].textContent == params.companyName) {
+//             return options[i].getAttribute('value')
+//           }
+//         }
+//       }, {id: selectId, companyName: req.body.company})
+//       await page.select(`select#${selectId}`, companyId),
 
-      await page.waitForSelector(`input#input_${companyId}`)
-      await page.click(`input#input_${companyId}`)
-      await page.keyboard.type(req.body.client)
-      await page.click(`#next-cart-step-${serviceId}`)
-      // await page.waitFor(30000)
+//       await page.waitForSelector(`input#input_${companyId}`)
+//       await page.click(`input#input_${companyId}`)
+//       await page.keyboard.type(req.body.client)
+//       await page.click(`#next-cart-step-${serviceId}`)
+//       // await page.waitFor(30000)
 
-      try {
-        await page.waitForSelector('.cost-detail')
-        const amount = await page.$eval('.cost-detail', el => el.innerText)
-        const date = await page.$eval(`#div_cont${serviceId} .cost h4`, el => el.innerText)
-        console.log({
-          amount: amount,
-          date: date
-        })
+//       try {
+//         await page.waitForSelector('.cost-detail')
+//         const amount = await page.$eval('.cost-detail', el => el.innerText)
+//         const date = await page.$eval(`#div_cont${serviceId} .cost h4`, el => el.innerText)
+//         console.log({
+//           amount: amount,
+//           date: date
+//         })
   
-        return res.json({
-          amount: amount,
-          date: date
-        })
-      } catch (error) {
-        await page.waitForSelector('#help-modal')
+//         return res.json({
+//           amount: amount,
+//           date: date
+//         })
+//       } catch (error) {
+//         await page.waitForSelector('#help-modal')
 
-        const amount = await page.$eval('#help-modal p', el => el.innerText)
-        console.log({
-          amount: amount
-        })
+//         const amount = await page.$eval('#help-modal p', el => el.innerText)
+//         console.log({
+//           amount: amount
+//         })
   
-        return res.json({
-          amount: amount
-        })
-      }
+//         return res.json({
+//           amount: amount
+//         })
+//       }
 
-    } catch (error) {
-      console.log(error)
-      return res.json({
-          error
-      })
-    } finally {
-      await closeConnection(page, browser);
-    }
-  } else {
-    return res.json({
-        error: 'Falta alguna variable.'
-    })
-  }
+//     } catch (error) {
+//       console.log(error)
+//       return res.json({
+//           error
+//       })
+//     } finally {
+//       await closeConnection(page, browser);
+//     }
+//   } else {
+//     return res.json({
+//         error: 'Falta alguna variable.'
+//     })
+//   }
 
-})
+// })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
